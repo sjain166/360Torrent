5ecfffea2f0af421f10c61ae08f6b4a19e8d542d
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2025-04-06 10:59:49 -0500

    diff plotting code, and color coded

--- scripts/visualization/test1.py
+++ scripts/visualization/test1.py
@@ -1,13 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from matplotlib.lines import Line2D
 
 import os
 import json
 
-DATA_DIR_BASELINE = "C:\\Users\\soula\\OneDrive\\Desktop\\3.0 (RF + RND)\\json\\"
-DATA_DIR_SOL = "C:\\Users\\soula\\OneDrive\\Desktop\\4.0\\json\\"
+# DATA_DIR_BASELINE = "C:\\Users\\soula\\OneDrive\\Desktop\\light1_plot\\3.0 (RF + RND)\\json\\"
+# DATA_DIR_SOL = "C:\\Users\\soula\\OneDrive\\Desktop\\light1_plot\\4.0\\json\\"
+# EVENTS_FILE = "C:\\Users\\soula\\OneDrive\\Desktop\\Programming\\CS525\\360Torrent\\data\\light1_workload\\events.json"
+# events_fs = open(EVENTS_FILE, 'r')
+# events_data = json.load(events_fs)
 
+
+DATA_DIR_BASELINE = "C:\\Users\\soula\\OneDrive\\Desktop\\heavy2_plot\\1.0 (RF + RND)\\json\\"
+DATA_DIR_SOL = "C:\\Users\\soula\\OneDrive\\Desktop\\heavy2_plot\\1.0\\json\\"
+EVENTS_FILE = "C:\\Users\\soula\\OneDrive\\Desktop\\Programming\\CS525\\360Torrent\\data\\heavy2_workload\\events.json"
+events_fs = open(EVENTS_FILE, 'r')
+events_data = json.load(events_fs)
 
 
 # PLot number of download failures per client
@@ -16,7 +26,6 @@ DATA_DIR_SOL = "C:\\Users\\soula\\OneDrive\\Desktop\\4.0\\json\\"
 files = os.listdir(DATA_DIR_BASELINE)
 files2 = os.listdir(DATA_DIR_SOL)
 # files2 = files
-REGION = "W"
 
 regions_to_vms = { 
             "W": [f"vm{i:02d}" for i in [1,2,3,4,5]],
@@ -28,16 +37,11 @@ def vm_to_region(vm):
     res = [ r for r, arr in regions_to_vms.items() if vm in arr]
     return res[0]
 
-
-EVENTS_FILE = "C:\\Users\\soula\\OneDrive\\Desktop\\Programming\\CS525\\360Torrent\\data\\light1_workload\\events.json"
-events_fs = open(EVENTS_FILE, 'r')
-events_data = json.load(events_fs)
-
 axes = []
 
 for i, (region, _ ) in enumerate(regions_to_vms.items()):
     fig, ax = plt.subplots(1, 1)
-    ax.set_title(f"Download latencies of each video in region {region} over successive requests")
+    ax.set_title(f"Download (s) of Each Video in Region {region} Over Successive Requests")
     ax.set_xlabel(f"Download Count in {region}")
     ax.set_ylabel("Download Latency (s)")
     ax.set_ylim(0,200)
@@ -55,6 +59,13 @@ def recover_timestamp(vmname, video):
                     return d["event"]["time"]
 
 
+# Each video has its own color, assuming 20 videos is enough
+cmap = plt.get_cmap('tab20')  # 'tab20' gives 20 distinct colors
+colors = [cmap(i) for i in range(20)]
+
+show_video_range = range(0,12)
+
+diff_latencies_per_region = []
 
 for system, dir, dirfiles in [("Baseline", DATA_DIR_BASELINE, files), ("Solution", DATA_DIR_SOL, files2)]:
     # Each element will be a map of videoN: [] latencies in that region
@@ -82,23 +93,91 @@ for system, dir, dirfiles in [("Baseline", DATA_DIR_BASELINE, files), ("Solution
             for vid, times in videos.items():
                 latencies_per_region[region][vid]  = sorted(latencies_per_region[region][vid], key=lambda x: x[0])
 
-        for ax_i, (region, _) in enumerate(regions_to_vms.items()):
-            for name, vid_data in latencies_per_region[region].items():
-                timestamps = []
-                latencys = []
-                nth_download = []
-                vms = []
-                # print(f" {len(vid_data)=} for region {name}")
-                for i, (timestamp, latency, vm) in enumerate(vid_data):
-                    timestamps.append(timestamp)
-                    latencys.append(latency)
-                    nth_download.append(i)
-                    vms.append(vm)
-                
-                color="blue"
-                if system=="Baseline": color="green"
-                axes[ax_i].scatter(nth_download, latencys, color=color)
-                axes[ax_i].plot(nth_download, latencys, color=color)
+    for ax_i, (region, _) in enumerate(regions_to_vms.items()):
+        for name, vid_data in latencies_per_region[region].items():
+            timestamps = []
+            latencys = []
+            nth_download = []
+            vms = []
+            # print(f" {len(vid_data)=} for region {name}")
 
+            vid_outlier = False
+            for i, (timestamp, latency, vm) in enumerate(vid_data):
+                if latency > 150: vid_outlier = True
+                timestamps.append(timestamp)
+                latencys.append(latency)
+                nth_download.append(i)
+                vms.append(vm)
+
+            if not vid_outlier:
+                vidnum = int(name[5:])
+                if vidnum in show_video_range:
+                    color = colors[vidnum]
+
+                    if system == "Baseline":
+                        axes[ax_i].scatter(nth_download, latencys, color=color)
+                        axes[ax_i].plot(nth_download, latencys, color=color, linestyle="-", label=name)
+                    else:
+                        axes[ax_i].scatter(nth_download, latencys, color=color)
+                        axes[ax_i].plot(nth_download, latencys, color=color, linestyle="--", label='__nolegend__')
+
+    diff_latencies_per_region.append(latencies_per_region)
+
+for ax in axes:
+    handles, labels = ax.get_legend_handles_labels()
+    # Sort legend items alphabetically (or however you like)
+    sorted_items = sorted(zip(labels, handles), key=lambda x: int(x[0][5:]))  # sort by label
+    labels, handles = zip(*sorted_items)
+    
+    video_legend = ax.legend(handles, labels, loc="upper right")
+
+    style_legend = [
+        Line2D([0], [0], color='black', linestyle='-', label='Baseline'),
+        Line2D([0], [0], color='black', linestyle='--', label='Solution')
+    ]
+    ax.legend(handles=style_legend, loc="upper left")
+    ax.add_artist(video_legend)
+
+plt.show()
+
+
+
+axes = []
+
+for i, (region, _ ) in enumerate(regions_to_vms.items()):
+    fig, ax = plt.subplots(1, 1)
+    ax.set_title(f"Improvement of Solution over Baseline in Region {region} Over Successive Requests")
+    ax.set_xlabel(f"Download Count in {region}")
+    ax.set_ylabel("Latency Improvement (s)")
+    ax.set_ylim(-100,100)
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    axes.append(ax)
+
+show_video_range = range(0,20)
+for ax_i, (region, _) in enumerate(regions_to_vms.items()):
+        
+        for (bname, bvid_data), (sname, svid_data) in zip(diff_latencies_per_region[0][region].items(), diff_latencies_per_region[1][region].items()):
+
+            vid_outlier = False
+
+            nth_download = []
+            diffs = []
+
+            for (i, (_ , blatency, _)), (j, (_, slatency, _)) in zip(enumerate(bvid_data), enumerate(svid_data)):
+                # if blatency or slatency > 150: vid_outlier = True
+                diffs.append(blatency - slatency) # So more positive is better
+                nth_download.append(i)
+
+            print(diffs)
+            # if not vid_outlier:
+            vidnum = int(bname[5:])
+            if vidnum in show_video_range:
+                color = colors[vidnum]
+
+                axes[ax_i].scatter(nth_download, diffs, color=color)
+                axes[ax_i].plot(nth_download, diffs, color=color, linestyle="-", label=bname)
+
+for ax in axes:
+    ax.legend()
 
 plt.show()
